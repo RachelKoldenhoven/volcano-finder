2569377fde4dc7581f88904a99b5e49d825ee096
RachelKoldenhoven <rachel@koldenfrozen.com>
2018-04-26 16:51:58 -0600

    mark search location, calc distance, get volcano name

--- src/Map.js
+++ src/Map.js
@@ -24,33 +24,10 @@ export default class Map extends Component {
             const maps = google.maps;
             const mapRef = this.refs.map;
             const node = ReactDOM.findDOMNode(mapRef);
-            const geocoder = new maps.Geocoder();
-
 
             let zoom = 2;
             let lat = 37.774929;
             let lng = -122.419416;
-
-            if (this.props.location !== '') {
-                geocoder.geocode({'address': this.props.location}, ((results, status) => {
-                    if (status === google.maps.GeocoderStatus.OK) {
-                        const userLocation = new maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-                        let minDistance = 10000000;
-                        let closestVolcano = "";
-                        for (let volcano of volcanoes) {
-                            let volcanoLoc = new google.maps.LatLng(volcano.Latitude, volcano.Longitude);
-                            let distance = google.maps.geometry.spherical.computeDistanceBetween(userLocation, volcanoLoc);
-                            if (distance <= minDistance) {
-                                minDistance = distance;
-                                closestVolcano = volcano;
-                                console.log(closestVolcano);
-                            }
-                        }
-                    } else {
-                        alert("Geocode was not successful for the following reason: " + status);
-                    }
-                }))
-            }
 
             const center = new maps.LatLng(lat, lng);
             const mapConfig = Object.assign({}, {
@@ -68,8 +45,46 @@ export default class Map extends Component {
                 })
             });
 
+            if (this.props.location !== '') {
+                this.loadSearchResult();
+            }
         }
+    }
 
+    loadSearchResult() {
+        const {google} = this.props;
+        const maps = google.maps;
+        const geocoder = new maps.Geocoder();
+
+        geocoder.geocode({'address': this.props.location}, ((results, status) => {
+            if (status === google.maps.GeocoderStatus.OK) {
+                const userLocation = new maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+                let minDistance = 10000000;
+                let closestVolcano = "";
+                for (let volcano of volcanoes) {
+                    let volcanoLoc = new google.maps.LatLng(volcano.Latitude, volcano.Longitude);
+                    let distance = google.maps.geometry.spherical.computeDistanceBetween(userLocation, volcanoLoc);
+                    if (distance <= minDistance) {
+                        minDistance = distance;
+                        closestVolcano = volcano;
+                    }
+                }
+
+                const displayDistance = Math.round(minDistance / 1000);
+                console.log(displayDistance);
+                const displayName = closestVolcano.Name;
+                console.log(displayName);
+                this.map.setCenter(results[0].geometry.location);
+                this.map.setZoom(6);
+                var marker = new google.maps.Marker({
+                    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                    map: this.map,
+                    position: results[0].geometry.location
+                });
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        }))
     }
 
     render() {
